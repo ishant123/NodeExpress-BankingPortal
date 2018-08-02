6a223f770018333a6b462b689dae0b6fb8f6070c
ishant123 <ishant.wadhwa5@gmail.com>
2018-08-02 13:44:50 +0530

    Change Express Name

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-const Express = require('express');
+const express = require('express');
 
-const app = Express();
+const app = express();
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
-app.use(Express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>res.render('index',{title:'Index'}));
 
