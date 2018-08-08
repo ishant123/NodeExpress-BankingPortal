eff2156223759dc6e38eccca62edcfc50ecbad70
ishant123 <ishant.wadhwa5@gmail.com>
2018-08-08 13:07:37 +0530

    module4

--- src/app.js
+++ src/app.js
@@ -30,4 +30,16 @@ app.post('/transfer',(req,res)=>{
     res.render('transfer',{message:'Transfer Complete'})
 });
 
+app.get('/payment',(req.res)=>res.render('payment',{account:accounts.credit}));
+app.post('/payment',(req,res)=>{
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount,10);
+    const accountsJSON = JSON.stringify(accounts,null,4);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
+    res.render('payment',{message:'Payment Successful', account:accounts.credit})
+});
+
+
+
+
 app.listen(3000,()=>console.log('PS Application is running on Port 3000'));
