bfa7c6ebf9b7889c927d589be2ac6e1f4fa1096c
ishant123 <ishant.wadhwa5@gmail.com>
2018-08-08 13:15:06 +0530

    module4

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.post('/transfer',(req,res)=>{
     accounts[req.body.to].balance =parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount,10);
     const accountsJSON = JSON.stringify(accounts,null,4);
     fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
-    res.render('transfer',{message:'Transfer Complete'})
+    res.render('transfer',{message:'Transfer Completed'});
 });
 
 app.get('/payment',(req,res)=>res.render('payment',{account:accounts.credit}));
