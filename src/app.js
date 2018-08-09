b98d72b7f4f1800c7319f736e935c4f7a85fd1ce
ishant123 <ishant.wadhwa5@gmail.com>
2018-08-09 13:59:32 +0530

    module6

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const {accounts, users, writeJSON} = require('./data')
+const {accounts, users, writeJSON} = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
 const app = express();
 
@@ -12,25 +14,10 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended:true}));// what it will work?
 
 app.get('/',(req,res)=>res.render('index',{title:'Account Summary' , accounts}));
-app.get('/savings',(req,res)=>res.render('account',{account : accounts.savings}));
-app.get('/checking',(req,res)=>res.render('account',{account: accounts.checking}));
-app.get('/credit',(req,res)=>res.render('account',{account: accounts.credit}));
-app.get('/profile',(req,res)=>res.render('profile',{user:users[0]}));
-app.get('/transfer',(req,res)=>res.render('transfer'));
-app.post('/transfer',(req,res)=>{
-    accounts[req.body.from].balance =parseInt(accounts[req.body.from].balance) - parseInt(req.body.amount,10);
-    accounts[req.body.to].balance =parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount,10);
-    writeJSON();
-    res.render('transfer',{message:'Transfer Completed'});
-});
+app.use('/account',accountRoutes);
+app.use('/services',servicesRoutes);
 
-app.get('/payment',(req,res)=>res.render('payment',{account:accounts.credit}));
-app.post('/payment',(req,res)=>{
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount,10);
-    writeJSON();
-    res.render('payment',{message:'Payment Successful', account:accounts.credit})
-});
+app.get('/profile',(req,res)=>res.render('profile',{user:users[0]}));
 
 
 
